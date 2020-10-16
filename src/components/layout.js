a6a72f78d33e80baf30fbffce215e3b091a0747b
francijpg <hellofrancijpg@gmail.com>
2020-10-16 17:19:15 -0300

    stylize layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,56 @@
 import React from "react"
 import Helmet from "react-helmet"
+import { Global, css } from "@emotion/core"
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Global
+        styles={css`
+          html {
+            font-size: 62.5%;
+            box-sizing: border-box;
+          }
+          *,
+          *:before,
+          *:after {
+            box-sizing: inherit;
+          }
+          body {
+            font-size: 1.6rem;
+            line-height: 2;
+            font-family: "Lato", sans-serif;
+          }
+          h1,
+          h2,
+          h3 {
+            margin: 0;
+            line-height: 1.5;
+          }
+          h1,
+          h2 {
+            text-align: center;
+            font-family: "Lato", sans-serif;
+            font-weight: 300;
+          }
+          h3 {
+            font-family: "Roboto", sans-serif;
+          }
+          ul {
+            list-style: none;
+            margin: 0;
+            padding: 0;
+          }
+          .contenedor {
+            max-width: 120rem;
+            margin: 0 auto;
+            width: 95%;
+          }
+          img {
+            max-width: 100%;
+          }
+        `}
+      />
       <Helmet>
         <title>Real Estate Gatsby</title>
         <meta
