48a22786a6ea3a09dd50f2fd88d71a4138c81b8e
francijpg <hellofrancijpg@gmail.com>
2020-10-22 12:42:33 -0300

    Update listadoPropiedades.js

--- src/components/listadoPropiedades.js
+++ src/components/listadoPropiedades.js
@@ -2,17 +2,26 @@ import React, { useState, useEffect } from "react"
 import usePropiedades from "../hooks/usePropiedades"
 import { css } from "@emotion/core"
 import PropiedadPreview from "./propiedadPreview"
-import listadoPropiedadesCSS from "../css/listadoPropiedades.module.css";
+import listadoPropiedadesCSS from "../css/listadoPropiedades.module.css"
+import useFiltro from "../hooks/useFiltro"
 
 const ListadoPropiedades = () => {
   const resultado = usePropiedades()
-  const [propiedades, guardarPropiedades] = useState([])
+  // propiedades mantener la referencia al filtro (usePropiedades) original (listar todo sin filtro)
+  const [propiedades] = useState(resultado);
+  const [filtradas, guardarFiltradas] = useState([])
+  // filtrado de propiedades
+  const { categoria, FiltroUI } = useFiltro()
 
   // console.log(resultado)
-  useEffect( () => {
-    guardarPropiedades(resultado);
-  },[])
-
+  useEffect(() => {
+    if (categoria) {
+      const filtro = propiedades.filter(propiedad => propiedad.categorias.nombre === categoria)
+      guardarFiltradas(filtro)
+    } else {
+      guardarFiltradas(propiedades)
+    }
+  }, [categoria, propiedades])
 
   return (
     <>
@@ -23,12 +32,10 @@ const ListadoPropiedades = () => {
       >
         Nuestras Propiedades
       </h2>
+      {FiltroUI()}
       <ul className={listadoPropiedadesCSS.propiedades}>
-        {propiedades.map(propiedad => (
-          <PropiedadPreview 
-            key={propiedad.id}
-            propiedad={propiedad}
-          />
+        {filtradas.map(propiedad => (
+          <PropiedadPreview key={propiedad.id} propiedad={propiedad} />
         ))}
       </ul>
     </>
